24de9af4b7480e3094f36d296161352aa0e0c185
Pablo Jose Gonzalez Granados <pjgg@Pablos-MacBook-Pro.local>
2018-05-31 17:18:00 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Go Redis Connector
+
+This allow you to make crud operation over redis
+
+## Install
+
+govendor fetch github.com/bq/redisConnector/connector
---
+++ connector/redisConnector.go
@@ -0,0 +1,79 @@
+package connector
+
+import (
+	"errors"
+	"sync"
+	"time"
+
+	redis "gopkg.in/redis.v5"
+)
+
+var (
+	ErrNoSuchEntity = errors.New("redis: no such entity")
+)
+
+type Unmarshal func(bytes []byte) error
+
+type redisConnector struct {
+	conn *redis.Client
+}
+
+type RedisCacher interface {
+	Ping() error
+	GetObject(key string, f Unmarshal) error
+	RemoveObject(keys string) error
+	CreateObject(key string, bytes []byte, expirationTime time.Duration) error
+	SetExpiration(key string, expirationTime time.Duration)
+}
+
+var onceRedis sync.Once
+var rConnector redisConnector
+
+func NewRedisSingleInstanceConnector(connectionChain string) RedisCacher {
+	onceRedis.Do(func() {
+		rConnector.conn = redis.NewClient(&redis.Options{
+			Addr: connectionChain,
+		})
+
+	})
+
+	return &rConnector
+}
+
+func NewRedisConnector(connectionChain string) RedisCacher {
+	rConnector := new(redisConnector)
+	rConnector.conn = redis.NewClient(&redis.Options{
+		Addr: connectionChain,
+	})
+	return rConnector
+}
+
+func (r *redisConnector) Ping() (err error) {
+	_, err = r.conn.Ping().Result()
+	return
+}
+
+func (r *redisConnector) GetObject(key string, f Unmarshal) (err error) {
+	bytes, e := r.conn.Get(key).Bytes()
+	if e != nil {
+		return ErrNoSuchEntity
+	}
+
+	if err = f(bytes); err != nil {
+		return errors.New("redis: error while unmarshal: " + err.Error())
+	}
+
+	return err
+}
+
+func (r *redisConnector) RemoveObject(key string) error {
+	return r.conn.Del(key).Err()
+}
+
+func (r *redisConnector) CreateObject(key string, bytes []byte, expirationTime time.Duration) error {
+	return r.conn.Set(key, bytes, expirationTime).Err()
+}
+
+func (r *redisConnector) SetExpiration(key string, expirationTimeInSec time.Duration) {
+	r.conn.ExpireAt(key, time.Now().Local().Add(expirationTimeInSec))
+}
